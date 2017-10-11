b64ad650f69d1f43654208e26d413cbb83de8ab8
Sergio <https://plaroser@bitbucket.org/plaroser/pruebaandroid.git>
2017-10-11 18:44:28 +0200

    Segunda parte del ejercicio terminada

--- DDII_Practica_Salario/Test/Program.cs
+++ DDII_Practica_Salario/Test/Program.cs
@@ -12,28 +12,36 @@ namespace DDII_Practica_Salario
         public static float IMPUESTOS = 17;
         static void Main(string[] args)
         {
+            bool continuar;
             String nombre;
             String apellidos;
             List<Persona> lista = new List<Persona>();
             Persona p1;
-            Console.WriteLine("Hola, introduzca el nombre de la persona: ");
-            nombre = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Introduzca los apellidos: ");
-            apellidos = Console.ReadLine();
-            p1 = new Persona(nombre, apellidos);
-            Console.Clear();
-            Console.WriteLine("Introduzca las horas trabajadas:");
-            p1.Horas = validarFloat();
-            Console.Clear();
-            Console.WriteLine("Introduzca el precio por hora:");
-            p1.PrecioHora = validarFloat();
-            lista.Add(p1);
+            do
+            {
+                Console.Clear();
+                continuar = false;
+                Console.WriteLine("Hola, introduzca el nombre de la persona: ");
+                nombre = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Introduzca los apellidos: ");
+                apellidos = Console.ReadLine();
+                p1 = new Persona(nombre, apellidos);
+                Console.Clear();
+                Console.WriteLine("Introduzca las horas trabajadas:");
+                p1.Horas = validarFloat();
+                Console.Clear();
+                Console.WriteLine("Introduzca el precio por hora:");
+                p1.PrecioHora = validarFloat();
+                lista.Add(p1);
+                Console.Clear();
+                Console.WriteLine("¿Quieres introducir mas empleados?");
+                continuar = validarS_N();
+            } while (continuar);
             imprimirSalario(lista);
-
         }
 
-        static int validarEnteros()
+        private static int validarEnteros()
         {
             int aux=0;
             bool esCorrecto;
@@ -55,7 +63,7 @@ namespace DDII_Practica_Salario
             } while (!esCorrecto);
             return aux;
         }
-        static float validarFloat()
+        private static float validarFloat()
         {
             float aux = 0;
             bool esCorrecto;
@@ -81,11 +89,32 @@ namespace DDII_Practica_Salario
         {
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine(("Nombre y apellidos".PadRight(40, '_')) + "Salario".PadLeft(15, '_')+"\n");
+            Console.WriteLine(("\tNombre y apellidos".PadRight(40, '-')) + "Salario".PadLeft(35, '-')+"\n");
             foreach (Persona p in listaPersonas)
             {
-                Console.WriteLine((p.toString().PadRight(40, '_')) + p.salario().ToString().PadLeft(15, '_')+"\n");
+                Console.WriteLine((("\t"+p.toString()).PadRight(40, '-')) + (p.salario() + " Eur").ToString().PadLeft(35, '-')+"\n");
             }
+        }
+        private static bool validarS_N()
+        {
+            bool esCorrecto;
+            bool aux;
+            string stringAux;
+            Console.Clear();
+            do
+            {
+                esCorrecto = false;
+                Console.Write("Introduzca \"s\" para SI o \"n\" para NO:");
+                stringAux=Console.ReadLine().ToLower();
+                esCorrecto = (stringAux == "s"||stringAux=="n");
+                if (!esCorrecto)
+                {
+                    Console.Clear();
+                    Console.WriteLine("ERROR no has introducido una opción valida.");
+                }
+                aux = stringAux == "s";
+            } while (!esCorrecto);
+            return aux;
         }
     }
 }
