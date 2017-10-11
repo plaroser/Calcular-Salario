9793fe2b77a13a1d3f791d1591c2500d6d62f507
Sergio <https://plaroser@bitbucket.org/plaroser/pruebaandroid.git>
2017-10-11 17:23:41 +0200

    Persona creado

---
+++ DDII_Practica_Salario/Models/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDII_Practica_Salario.Models
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellidos;
+
+        public Persona(string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        
+        public string toString()
+        {
+            return Apellidos + ", " + nombre;
+        }
+        
+    }
+}
